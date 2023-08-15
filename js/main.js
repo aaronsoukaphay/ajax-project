22b0234b1ebba83006259f6d244ba924abc25529
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-08-15 04:34:30 +0000

    created viewswap function to swap between home and search-results pages

--- js/main.js
+++ js/main.js
@@ -1,38 +1,10 @@
 const $ul = document.querySelector('ul');
 
-// function getSearchData(search) {
-//   const xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'https://api.jikan.moe/v4/anime?q=one-piece');
-//   xhr.responseType = 'json';
-//   xhr.addEventListener('load', () => {
-//     console.log(xhr.status);
-//     console.log(xhr.response);
-//     for (let i = 0; i < xhr.response.data.length; i++) {
-//       const $li = document.createElement('li');
-//       $li.setAttribute('class', 'column-third');
-
-//       const $img = document.createElement('img');
-//       $img.setAttribute('src', xhr.response.data[i].images.webp.image_url);
-//       $li.appendChild($img);
-
-//       const $p = document.createElement('p');
-//       $p.setAttribute('class', 'title');
-//       $p.textContent = xhr.response.data[i].title_english;
-//       $li.appendChild($p);
-
-//       $ul.appendChild($li);
-//     }
-//   });
-//   xhr.send();
-// }
-
-// getSearchData();
-
 const $searchButton = document.querySelector('.search-button');
 const $query = document.querySelector('#query');
 const jikan = 'https://api.jikan.moe/v4/anime?q=';
 
-function submitted(event) {
+function searchFor(event) {
   event.preventDefault();
   const url = jikan + $query.value;
   const xhr = new XMLHttpRequest();
@@ -58,6 +30,21 @@ function submitted(event) {
     }
   });
   xhr.send();
+  viewSwap('search-results');
 }
 
-$searchButton.addEventListener('click', submitted);
+$searchButton.addEventListener('click', searchFor);
+
+const $home = document.querySelector('.home');
+const $searchResults = document.querySelector('.search-results');
+
+function viewSwap(viewName) {
+  if (viewName === 'search-results') {
+    $home.className = 'home hidden';
+    $searchResults.className = 'search-results';
+  } else if (viewName === 'home') {
+    $searchResults.className = 'search-results hidden';
+    $home.className = 'home';
+  }
+  data.view = viewName;
+}
