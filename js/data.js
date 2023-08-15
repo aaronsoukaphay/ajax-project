6a3051f60b03e28599db76c50f51c3ab88d10a3f
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-08-15 17:49:33 +0000

    completed viewswap for top animes, watchlist, home, and details pages

--- js/data.js
+++ js/data.js
@@ -1,7 +1,20 @@
 /* exported data */
 let data = {
-  view: 'entry-form',
+  view: 'home',
   entries: [],
   editing: null,
   nextEntryId: 1
 };
+
+window.addEventListener('beforeunload', handleUnload);
+
+function handleUnload() {
+  const dataJSON = JSON.stringify(data);
+  localStorage.setItem('javscript-local-storage', dataJSON);
+
+  const previousDataJSON = localStorage.getItem('javascript-local-storage');
+
+  if (previousDataJSON !== null) {
+    data = JSON.parse(previousDataJSON);
+  }
+}
--- js/main.js
+++ js/main.js
@@ -2,7 +2,9 @@ const $ul = document.querySelector('ul');
 const $searchButton = document.querySelector('.search-button');
 const $query = document.querySelector('#query');
 const jikan = 'https://api.jikan.moe/v4/anime?q=';
-// const $form = document.querySelector('.form');
+const $form = document.querySelector('.form');
+
+$searchButton.addEventListener('click', searchFor);
 
 function searchFor(event) {
   event.preventDefault();
@@ -31,34 +33,91 @@ function searchFor(event) {
   });
   xhr.send();
   viewSwap('search-results');
+  toggleNoEntries();
 }
-
-$searchButton.addEventListener('click', searchFor);
 
 const $home = document.querySelector('.home');
 const $searchResults = document.querySelector('.search-results');
 const $headerTitle = document.querySelector('#header-title');
+const $details = document.querySelector('.details');
+const $watchlist = document.querySelector('.watchlist');
+const $topAnimes = document.querySelector('.top-animes');
 
 function viewSwap(viewName) {
   if (viewName === 'search-results') {
     $home.className = 'home hidden';
     $searchResults.className = 'search-results';
     $headerTitle.className = 'header-tab';
+    $watchlist.className = 'watchlist hidden';
+    $details.className = 'details hidden';
+    $topAnimes.className = 'top-animes hidden';
   } else if (viewName === 'home') {
     $searchResults.className = 'search-results hidden';
     $home.className = 'home';
     $headerTitle.className = 'header-tab hidden';
+    $watchlist.className = 'watchlist hidden';
+    $details.className = 'details hidden';
+    $topAnimes.className = 'top-animes hidden';
+  } else if (viewName === 'details') {
+    $details.className = 'details';
+    $searchResults.className = 'search-results hidden';
+    $home.className = 'home hidden';
+    $headerTitle.className = 'header-tab';
+    $watchlist.className = 'watchlist hidden';
+    $topAnimes.className = 'top-animes hidden';
+  } else if (viewName === 'watchlist') {
+    $details.className = 'details hidden';
+    $searchResults.className = 'search-results hidden';
+    $home.className = 'home hidden';
+    $headerTitle.className = 'header-tab';
+    $watchlist.className = 'watchlist';
+    $topAnimes.className = 'top-animes hidden';
+  } else if (viewName === 'top-animes') {
+    $details.className = 'details hidden';
+    $searchResults.className = 'search-results hidden';
+    $home.className = 'home hidden';
+    $headerTitle.className = 'header-tab';
+    $watchlist.className = 'watchlist hidden';
+    $topAnimes.className = 'top-animes';
   }
   data.view = viewName;
 }
 
+const $topAnimesTab = document.querySelector('#top-animes');
+const $watchlistTab = document.querySelector('#watchlist');
+
+$headerTitle.addEventListener('click', function () {
+  $form.reset();
+  viewSwap('home');
+});
+
+$topAnimesTab.addEventListener('click', function () {
+  viewSwap('top-animes');
+});
+
+$watchlistTab.addEventListener('click', function () {
+  viewSwap('watchlist');
+});
+
+const $noEntries = document.querySelector('.no-entries');
+const $liList = document.getElementsByClassName('column-third');
+// console.log($liList);
+
+function toggleNoEntries() {
+  if ($liList.length >= 1) {
+    $noEntries.className = 'no-entries hidden';
+  } else {
+    $noEntries.className = 'no-entries';
+  }
+}
+
 let counter = 0;
 const $imgList = document.querySelectorAll('.carousel');
-// console.log($imgList);
 
 const bannerID = setInterval(nextImg, 4000);
 
 function nextImg() {
+  clearInterval(bannerID);
   if (counter < 5) {
     // console.log('running');
     for (let i = 0; i < $imgList.length; i++) {
@@ -71,6 +130,5 @@ function nextImg() {
     counter++;
   } else {
     counter = 0;
-    clearInterval(bannerID);
   }
 }
