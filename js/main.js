8b9361fd052f742c4ce1aaf31c283bd34991a929
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-08-15 04:20:17 +0000

    got the search bar to work with api #1

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
-// const $searchList = document.querySelector('#search-list');
+const $ul = document.querySelector('ul');
 
 // function getSearchData(search) {
 //   const xhr = new XMLHttpRequest();
-//   xhr.open('GET', 'https://api.jikan.moe/v4/anime?q=bleach');
+//   xhr.open('GET', 'https://api.jikan.moe/v4/anime?q=one-piece');
 //   xhr.responseType = 'json';
 //   xhr.addEventListener('load', () => {
 //     console.log(xhr.status);
@@ -10,15 +10,54 @@
 //     for (let i = 0; i < xhr.response.data.length; i++) {
 //       const $li = document.createElement('li');
 //       $li.setAttribute('class', 'column-third');
-//       $searchList.appendChild('$li');
-//       const $img = document.createElement('img');
 
-//       $newLi.textContent = xhr.response.data[i].title_english;
-//       $searchList.appendChild($newLi);
-//       console.log($searchList);
+//       const $img = document.createElement('img');
+//       $img.setAttribute('src', xhr.response.data[i].images.webp.image_url);
+//       $li.appendChild($img);
+
+//       const $p = document.createElement('p');
+//       $p.setAttribute('class', 'title');
+//       $p.textContent = xhr.response.data[i].title_english;
+//       $li.appendChild($p);
+
+//       $ul.appendChild($li);
 //     }
 //   });
 //   xhr.send();
 // }
 
 // getSearchData();
+
+const $searchButton = document.querySelector('.search-button');
+const $query = document.querySelector('#query');
+const jikan = 'https://api.jikan.moe/v4/anime?q=';
+
+function submitted(event) {
+  event.preventDefault();
+  const url = jikan + $query.value;
+  const xhr = new XMLHttpRequest();
+  xhr.open('GET', url);
+  xhr.responseType = 'json';
+  xhr.addEventListener('load', () => {
+    // console.log(xhr.status);
+    // console.log(xhr.response);
+    for (let i = 0; i < xhr.response.data.length; i++) {
+      const $li = document.createElement('li');
+      $li.setAttribute('class', 'column-third');
+
+      const $img = document.createElement('img');
+      $img.setAttribute('src', xhr.response.data[i].images.webp.image_url);
+      $li.appendChild($img);
+
+      const $p = document.createElement('p');
+      $p.setAttribute('class', 'title');
+      $p.textContent = xhr.response.data[i].title_english;
+      $li.appendChild($p);
+
+      $ul.appendChild($li);
+    }
+  });
+  xhr.send();
+}
+
+$searchButton.addEventListener('click', submitted);
