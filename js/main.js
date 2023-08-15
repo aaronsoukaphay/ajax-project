8e0d2c5a214aafecc0115b39398c3d62d56ef9e7
Aaron Soukaphay <aaronsoukaphay@gmail.com>
2023-08-15 01:14:33 +0000

    Check that the example entries you just created in the unordered list are responsive for both mobile and desktop screens. #1

--- js/main.js
+++ js/main.js
@@ -1,20 +1,24 @@
-const $searchList = document.querySelector('#search-list');
+// const $searchList = document.querySelector('#search-list');
 
-function getSearchData(search) {
-  const xhr = new XMLHttpRequest();
-  xhr.open('GET', 'https://api.jikan.moe/v4/anime?q=bleach');
-  xhr.responseType = 'json';
-  xhr.addEventListener('load', () => {
-    // console.log(xhr.status);
-    // console.log(xhr.response);
-    for (let i = 0; i < xhr.response.data.length; i++) {
-      const $newLi = document.createElement('li');
+// function getSearchData(search) {
+//   const xhr = new XMLHttpRequest();
+//   xhr.open('GET', 'https://api.jikan.moe/v4/anime?q=bleach');
+//   xhr.responseType = 'json';
+//   xhr.addEventListener('load', () => {
+//     console.log(xhr.status);
+//     console.log(xhr.response);
+//     for (let i = 0; i < xhr.response.data.length; i++) {
+//       const $li = document.createElement('li');
+//       $li.setAttribute('class', 'column-third');
+//       $searchList.appendChild('$li');
+//       const $img = document.createElement('img');
 
-      $newLi.textContent = xhr.response.data[i].title_english;
-      $searchList.appendChild($newLi);
-    }
-  });
-  xhr.send();
-}
+//       $newLi.textContent = xhr.response.data[i].title_english;
+//       $searchList.appendChild($newLi);
+//       console.log($searchList);
+//     }
+//   });
+//   xhr.send();
+// }
 
-getSearchData();
+// getSearchData();
